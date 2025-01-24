8a2f99120bcb2160d87fb273392219229ba0b9ab
Tomas Tamantini <tomasbft@gmail.com>
2025-01-24 13:51:07 -0300

    Display player's cards

--- build/components/my_info.js
+++ build/components/my_info.js
@@ -11,6 +11,10 @@ export default class MyInfoComponent {
             this.myCardsContainer.removeChild(this.myCardsContainer.firstChild);
         }
     }
+    cardToStr(card) {
+        const suitSymbols = { 'd': '♦', 'c': '♣', 'h': '♥', 's': '♠' };
+        return `${card.rank}${suitSymbols[card.suit]}`;
+    }
     update(state) {
         var _a;
         this.reset();
@@ -20,5 +24,23 @@ export default class MyInfoComponent {
         const text = `${me.playerId} - ${me.numPoints}pts`;
         if (this.myInfoSpan)
             this.myInfoSpan.innerHTML = text;
+        const cards = state.myCards;
+        if (cards) {
+            cards.forEach(card => {
+                var _a;
+                const cardDiv = document.createElement('div');
+                cardDiv.classList.add('card');
+                cardDiv.classList.add('card-front');
+                if (card.suit === 'd' || card.suit === 'h') {
+                    cardDiv.classList.add('red');
+                }
+                else {
+                    cardDiv.classList.add('black');
+                }
+                const text = this.cardToStr(card);
+                cardDiv.innerHTML = text;
+                (_a = this.myCardsContainer) === null || _a === void 0 ? void 0 : _a.appendChild(cardDiv);
+            });
+        }
     }
 }
--- src/components/my_info.ts
+++ src/components/my_info.ts
@@ -1,3 +1,4 @@
+import { Card } from "../card";
 import { GlobalState } from "../state";
 
 export default class MyInfoComponent {
@@ -11,11 +12,33 @@ export default class MyInfoComponent {
         }
     }
 
+    private cardToStr(card: Card) {
+        const suitSymbols = { 'd': '♦', 'c': '♣', 'h': '♥', 's': '♠' };
+        return `${card.rank}${suitSymbols[card.suit]}`;
+    }
+
     public update(state: GlobalState) {
         this.reset();
         const me = state.sharedGameState?.players.find(p => p.playerId === state.playerId);
         if (!me) return;
         const text = `${me.playerId} - ${me.numPoints}pts`;
         if (this.myInfoSpan) this.myInfoSpan.innerHTML = text;
+        const cards = state.myCards;
+        if (cards) {
+            cards.forEach(card => {
+                const cardDiv = document.createElement('div');
+                cardDiv.classList.add('card');
+                cardDiv.classList.add('card-front');
+                if (card.suit === 'd' || card.suit === 'h') {
+                    cardDiv.classList.add('red');
+                }
+                else {
+                    cardDiv.classList.add('black');
+                }
+                const text = this.cardToStr(card);
+                cardDiv.innerHTML = text;
+                this.myCardsContainer?.appendChild(cardDiv);
+            })
+        }
     }
 }
