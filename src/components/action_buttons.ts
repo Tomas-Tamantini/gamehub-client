3e31dd9558790efa9363aac16b13e87c9b0e7697
Tomas Tamantini <tomasbft@gmail.com>
2025-01-30 06:45:59 -0300

    Display action buttons during game only

--- src/components/action_buttons.ts
+++ src/components/action_buttons.ts
@@ -11,22 +11,42 @@ export default class ActionButtonsComponent {
         this.passBtn?.addEventListener('click', () => this.gameService.makeMove([]));
     }
 
+    private isMidGame(state: GlobalState) {
+        return (
+            state.playerId &&
+            state.roomId &&
+            state.sharedGameState &&
+            state.sharedGameState.status !== GameStatus.END_GAME
+        )
+    }
+
     private isMyTurn(state: GlobalState) {
         return state.sharedGameState?.status === GameStatus.AWAIT_PLAYER_ACTION &&
             state.sharedGameState?.currentPlayerId === state.playerId;
     }
 
     private updatePassBtn(state: GlobalState) {
-        if (this.isMyTurn(state)) this.passBtn?.removeAttribute('disabled');
-        else this.passBtn?.setAttribute('disabled', 'true');
+        if (!this.isMidGame(state)) {
+            this.passBtn?.classList.add('hidden');
+        }
+        else {
+            this.passBtn?.classList.remove('hidden');
+            if (this.isMyTurn(state)) this.passBtn?.removeAttribute('disabled');
+            else this.passBtn?.setAttribute('disabled', 'true');
+        }
     }
 
     private updateMakeMoveBtn(state: GlobalState) {
-        const numSelectedCards = state.selectedCards?.length ?? 0;
-        if (this.isMyTurn(state) && numSelectedCards > 0) this.makeMoveBtn?.removeAttribute('disabled');
-        else this.makeMoveBtn?.setAttribute('disabled', 'true');
+        if (!this.isMidGame(state)) {
+            this.makeMoveBtn?.classList.add('hidden');
+        }
+        else {
+            this.makeMoveBtn?.classList.remove('hidden');
+            const numSelectedCards = state.selectedCards?.length ?? 0;
+            if (this.isMyTurn(state) && numSelectedCards > 0) this.makeMoveBtn?.removeAttribute('disabled');
+            else this.makeMoveBtn?.setAttribute('disabled', 'true');
+        }
     }
-
 
     public update(state: GlobalState) {
         this.updatePassBtn(state);
