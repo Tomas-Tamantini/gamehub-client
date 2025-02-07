11c67fe4fd64436482a92ccd9adb14165c2acb70
Tomas Tamantini <tomasbft@gmail.com>
2025-02-07 12:50:51 -0300

    Extract case conversion methods

--- src/socket_service.ts
+++ src/socket_service.ts
@@ -1,31 +1,4 @@
-function camelToSnake(obj: any): any {
-    if (Array.isArray(obj)) {
-        return obj.map(camelToSnake);
-    }
-    else if (obj && typeof obj === "object") {
-        return Object.fromEntries(
-            Object.entries(obj).map(([key, value]) => [
-                key.replace(/([A-Z])/g, "_$1").toLowerCase(),
-                camelToSnake(value),
-            ])
-        );
-    }
-    else return obj;
-}
-
-function snakeToCamel(obj: any): any {
-    if (Array.isArray(obj)) {
-        return obj.map(snakeToCamel);
-    } else if (obj && typeof obj === "object") {
-        return Object.fromEntries(
-            Object.entries(obj).map(([key, value]) => [
-                key.replace(/_([a-z])/g, (_, char) => char.toUpperCase()),
-                snakeToCamel(value),
-            ])
-        );
-    }
-    return obj;
-}
+import { camelToSnake, snakeToCamel } from "./case_conversion";
 
 export default class SocketService {
     private ws?: WebSocket;
